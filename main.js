fe6a8c74d4fc3f23caf1d38b7609065968907882
dayron-rd <ing.dayron.rd@gmail.com>
2025-02-06 09:50:46 +0100

    Existian errores ortográficos en la etiqueta

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Declaración de variables de los botonoes
+// Declaración de variables de los botones
 const botonAgrega = document.getElementById('superButton');
 const botonReset = document.getElementById('resetButton');
 const botonsuma = document.getElementById('sumatoria');
@@ -27,7 +27,7 @@ function menu() {
 }
 function reiniciar(evento){
     evento.preventDefault();
-        document.getElementById('lista').innerHTML = "";vbgg
+        document.getElementById('lista').innerHTML = "";
         document.getElementsByClassName('texto')[0].innerHTML = "";
         suma = 0;
         document.getElementById('superButton').style.display = "block";
